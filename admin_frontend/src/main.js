0bc9d90ce287d68ec58287cd4954fa7aa3eb49af
reza-ak <reeza.dev@gmail.com>
2023-11-01 22:03:36 +0330

    Add Bootstrap V5.3.2

--- admin_frontend/src/main.js
+++ admin_frontend/src/main.js
@@ -1,6 +1,13 @@
+// import css file
 import './assets/main.css'
 
+// import bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import vue
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
