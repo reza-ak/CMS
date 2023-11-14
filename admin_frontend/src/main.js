3639384f02e3a1844bdd6d0f61fdb4278ea29ea4
reza-ak <reeza.dev@gmail.com>
2023-11-14 14:20:18 +0330

    Add pinia V2.1.7

--- admin_frontend/src/main.js
+++ admin_frontend/src/main.js
@@ -1,13 +1,18 @@
-// import css file
+// sass
 import './assets/main.scss'
 
-// import bootstrap
+// bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import vue
+// vue
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// pinia
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+
 const app = createApp(App);
+app.use(pinia);
 app.mount('#app');
