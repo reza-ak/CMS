b9263055cabc3101a5e0d5191eebc296bb197b8e
reza-ak <reeza.dev@gmail.com>
2023-12-05 21:29:50 +0330

    Display date and time (home page) - admin

---
+++ admin_frontend/src/components/pages/home/DateTime.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+
+// ====================== Date and time
+const time = ref(new Date());
+const weekday = [
+  "یکشنبه",
+  "دوشنبه",
+  "سه شنبه",
+  "چهارشنبه",
+  "پنجشنبه",
+  "جمعه",
+  "شنبه",
+];
+setInterval(() => {
+  time.value = new Date();
+}, 1000);
+</script>
+
+<template>
+  <div class="col-12 col-md-6 col-lg-3 pb-3 pb-lg-0">
+    <div
+      class="border bg-white d-flex justify-content-center align-items-center h-100 py-4"
+    >
+      <div class="card-body text-secondary text-center">
+        <div class="timeBox h2">
+          <span>{{ time.getSeconds("fa-IR") }}</span>
+          <span> : </span>
+          <span>{{ time.getMinutes("fa-IR") }}</span>
+          <span> : </span>
+          <span>{{ time.getHours("fa-IR") }}</span>
+        </div>
+        <div class="dateBox">
+          {{ weekday[time.getDay()] }},
+          {{
+            time.toLocaleDateString("fa-IR", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })
+          }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ admin_frontend/src/pages/home.vue
@@ -0,0 +1,15 @@
+<script setup>
+// ====================== components
+import DateTime from "@/components/pages/home/DateTime.vue";
+</script>
+
+<template>
+  <article>
+    <div class="row m-0 d-flex justify-content-between py-2 py-lg-3">
+      <!-- date and time -->
+      <DateTime />
+    </div>
+  </article>
+
+  <article>todo list</article>
+</template>
